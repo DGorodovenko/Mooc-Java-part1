2b2e3687ea1a77cb7d7a75b9562b7e25f93a5bcf
Dmytro Horodovenko <48045556+DGorodovenko@users.noreply.github.com>
2023-03-20 10:52:38 +0100

    Add files via upload

---
+++ part02-Part02_04.ComparingNumbers/src/main/java/Hello.java
@@ -0,0 +1,27 @@
+
+
+
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        int number = 0;
+
+        while (true) {
+            number = number + 1;
+
+            if (number >= 5) {
+                break;
+            }
+
+            if (number < 5) {
+                continue;
+            }
+
+            System.out.println(number + " ");
+        }
+
+    System.out.println(number + " ");
+    }
+}
